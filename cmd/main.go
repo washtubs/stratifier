b5a4f0ed63f0b7a56a131dd416ad953d7d936b61
David Milum <david.milum.89@gmail.com>
2015-09-17 20:39:39 -0400

    just add the main file

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// TODO: make this just take a file as the first arg and print the default stratification
+
+	// LONGTERM TODO:
+	// TODO: do I need a nice arg parsing lib?
+	// TODO: parse args
+	// NOTE: should be super configurable
+}
